45958cdd5f28f8db38bca1eda05a9994b205f93c
rnairk <nair.ranjit.k@gmail.com>
2023-09-30 16:58:28 +0530

    Fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/names/i);
+  const linkElement = screen.getByText(/List of Names/i);
   expect(linkElement).toBeInTheDocument();
 });
